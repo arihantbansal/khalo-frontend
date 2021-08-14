6a3b0124df188bbed7b248d35d784154024d741b
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2021-08-14 12:35:58 +0530

    Okay

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import {
 	Grid,
 	theme,
 } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "ColorModeSwitcher";
 import Home from "components/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
 import Login from "pages/Login";
+import Navbar from "components/Navbar";
 
 const App = () => {
 	return (
 		<ChakraProvider theme={theme}>
 			<Box textAlign="center" fontSize="xl">
 				<Grid minH="100vh" p={3}>
-					<ColorModeSwitcher justifySelf="flex-end" />
+					<Navbar />
 					<VStack spacing={8}>
 						<Router>
 							<Switch>
