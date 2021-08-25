d3135396f57a576ceb34cfb044e688a737b207a5
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2021-08-25 16:28:40 +0530

    updated about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -32,43 +32,15 @@ const About = () => {
 				direction={isNotSmallerScreen ? "row" : "column"}>
 				<Box textAlign="left">
 					<Text color={"gray.500"} maxW={"3xl"}>
-						Do you have the fear of losing your money in the stock market? Do
-						you want to try the market out without putting your money at stake?
-						Well,{" "}
-						<Text as={"span"} color={"primary.400"}>
-							StonkR
-						</Text>{" "}
-						is here for you!
+						Are you not in the mood to get out of bed, get dressed, and go out
+						to a restaurant?
 					</Text>
 					<Text color={"gray.500"} maxW={"3xl"} marginTop="20px">
-						Use the{" "}
+						Use{" "}
 						<Text as={"span"} color={"primary.400"}>
-							StonkR
+							khalo
 						</Text>{" "}
-						website to try mock trading using REAL stock market data from the
-						USA stock market. The{" "}
-						<Text as={"span"} color={"primary.400"}>
-							Dashboard
-						</Text>{" "}
-						page shows you your portfolio, which includes your investments and
-						assets.
-					</Text>
-					<Text color={"gray.500"} maxW={"3xl"} marginTop="20px">
-						Thinking about investing in mutual funds instead? Head over to the{" "}
-						<Text as={"span"} color={"primary.400"}>
-							Calculators
-						</Text>{" "}
-						page and check out how much returns you could get if you invest by
-						using our{" "}
-						<Text as={"span"} color={"primary.400"}>
-							SIP Calculator
-						</Text>{" "}
-						or the{" "}
-						<Text as={"span"} color={"primary.400"}>
-							Lumpsum Calculator
-						</Text>
-						. Even better, your profits are adjusted for inflation! So you know,
-						exactly how much your profits will be after maturity!
+						web app to order food from the best restaurants near you.
 					</Text>
 				</Box>
 
